52dd3085cc550136a18e49e746d0fc3e928574e6
KangMinHyeok <saigoyo@nate.com>
2019-06-13 11:21:56 +0900

    Update rec4item.py

--- 1B/rec4item.py
+++ 1B/rec4item.py
@@ -4,6 +4,7 @@ import sys
 
 sys.path.append('../..')
 
+import time
 import json
 import heapq
 import operator
@@ -24,17 +25,20 @@ import keras.backend as K
 from keras.utils import np_utils
 from theano import tensor as T
 
-PATH_TO_TRAIN = '../data/processed/rsc15_train_full_action_mini.txt'
-PATH_TO_TEST = '../data/processed/rsc15_test_action_mini.txt'
+# PATH_TO_TRAIN = '../data/processed/rsc15_train_full_action_mini.txt'
+# PATH_TO_TEST = '../data/processed/rsc15_test_action_mini.txt'
 
 
 # PATH_TO_TRAIN = '../data/processed/train_full_action_mini.txt'
 # PATH_TO_TEST = '../data/processed/test_full_action_mini.txt'
 
 
-PATH_TO_TRAIN = '../data/processed/train_full_action.txt'
-PATH_TO_TEST = '../data/processed/test_full_action.txt'
+# PATH_TO_TRAIN = '../data/processed/train_full_action.txt'
+# PATH_TO_TEST = '../data/processed/test_full_action.txt'
 
+
+PATH_TO_TRAIN = '../data/processed/train_full_action2.txt'
+PATH_TO_TEST = '../data/processed/test_full_action2.txt'
 
 # PATH_TO_TRAIN = '../data/processed/rsc15_train_full.txt'
 # PATH_TO_TEST = '../data/processed/rsc15_test.txt'
@@ -64,16 +68,22 @@ del(train)
 del(itemids)
 print(itemidMap)
 
-#user_id	session_id	timestamp	step	action_type	reference	platform	city	device	current_filters	impressions	prices	item_idx_x	action_idx	item_idx_y
-data = pd.read_csv(PATH_TO_TRAIN, sep='\t', 
-					usecols=[0,1,2,3, 4,5,10,12,13,14], 
-					dtype={0: str, 1:str, 2:str, 3:str, 4:str, 5:str, 10:str, 12:int, 13:int, 14:str})
-valid = pd.read_csv(PATH_TO_TEST, sep='\t', 
-					usecols=[0,1,2,3, 4,5,10,12,13,14], 
-					dtype={0: str, 1:str, 2:str, 3:str, 4:str, 5:str, 10:str, 12:int, 13:int, 14:str})
-data.columns = ['user_id', 'session_id', 'timestamp', 'step', 'action_type', 'reference', 'impressions', 'item_idx', 'action_idx', 'actions']
-valid.columns = ['user_id', 'session_id', 'timestamp', 'step', 'action_type', 'reference', 'impressions', 'item_idx', 'action_idx', 'actions']
+
+#user_id	session_id	timestamp	step	action_type	reference	platform	city	device			current_filters	impressions	prices		item_idx_x	action_idx	item_idx_y
+#0			1			2			3		4			5			6			7		8				9				10			11			12			13			14	
+#user_id	session_id	timestamp	step	action_type	platform	city		device	current_filters	impressions		prices		action_idx	reference	item_idx_x	item_idx_y
+# data = pd.read_csv(PATH_TO_TRAIN, sep='\t', 
+# 					usecols=[0,1,2,3, 4,10,12,13,14,15], 
+					# dtype={0: str, 1:str, 2:str, 3:str, 4:str, 10:str, 12:str, 13:str, 14:int, 15:str})
+data = pd.read_csv(PATH_TO_TRAIN, sep='\t', dtype={"item_idx_x":int})
+data = data.rename(index=str, columns={"item_idx_x": "item_idx", "item_idx_y":"actions"})
+valid = pd.read_csv(PATH_TO_TEST, sep='\t', dtype={"item_idx_x":int})
+valid = valid.rename(index=str, columns={"item_idx_x": "item_idx", "item_idx_y":"actions"})
+# data.columns = ['user_id', 'session_id', 'timestamp', 'step', 'action_type', 'impressions', 'action_idx', 'reference', 'item_idx', 'actions']
+#valid.columns = ['user_id', 'session_id', 'timestamp', 'step', 'action_type', 'impressions', 'action_idx', 'reference', 'item_idx', 'actions']
+
 # test = pd.read_csv(PATH_TO_TEST, sep='\t', dtype={'ItemId':np.float64})
+
 print("reading Done")
 # mask = test["ItemId"].isnull()
 # test = test[mask]
@@ -87,23 +97,30 @@ def bpr_loss(layer):
 	# Return a function
 	return loss
 
-data = data.iloc[:int(len(data)/2)]
-data['action_list'] = data.actions.apply(lambda x: np.array(x.split('|')).reshape(-1, 1) )
+max_itemid = data.item_idx.max()
+#data = data.iloc[:int(len(data)/2)]
+#data = data.iloc[:int(len(data)/10)]
+#data = data.iloc[:100]
+#valid = valid.iloc[:100]
+
+data['action_list'] = data.actions.apply(lambda x: np.array(list(map(int, x.split('|')))).reshape(-1, 1) )
 #data['action_list'] = data.actions.apply(lambda x: list(map( lambda y: [y], json.loads(x)[::-1])))
-valid['action_list'] = data.actions.apply(lambda x: np.array(x.split('|')).reshape(-1, 1) )
+valid['action_list'] = valid.actions.apply(lambda x: np.array(list(map(int, x.split('|')))).reshape(-1, 1) )
 #valid['action_list'] = valid.actions.apply(lambda x: list(map( lambda y: [y], json.loads(x)[::-1])))
 #data['impression_list'] = data.impressions.apply(lambda x: list(map(lambda y: itemidMap[y], x.split('|'))))
 #valid['impression_list'] = valid.impressions.apply(lambda x: list(map(lambda y: itemidMap[y], x.split('|'))))
 #print(data.action_list)
 #print(valid.action_list)
 #exit()
+
+
 action_len = len(data['action_list'].iloc[0])
 #impression_len = len(data['impression_list'].iloc[0])
-max_itemid = data.item_idx.max()
+
 
 embedding_dim = 50
 input_len = 1
-batch_size = 20
+batch_size = 150
 print(f"max item id: {max_itemid}")
 print(f"action len: {action_len}")
 
@@ -127,7 +144,7 @@ merged = concatenate([item_gru, action_dense], name='merge')
 
 final_dense =  Dense(max_itemid+1, activation='softmax', name="final_dense")(merged)
 
-final_drop = Dropout(0.9)(final_dense)
+final_drop = Dropout(0.5)(final_dense)
 final_dense2 = Dense(1, activation='softmax', name='final_dense2')(final_drop)
 model  = Model(inputs=[item_in, action_in], outputs=final_dense2)
 
@@ -154,13 +171,23 @@ print(actions.shape)
 #print("one hot shape")
 #print(one_hot_y.shape)
 model.fit([items[:-1], actions[:-1]], items[1:]#one_hot_y[1:]
-	, epochs=1, batch_size=batch_size, verbose=1)
+	, epochs=10, batch_size=batch_size, verbose=1)
 
+import random
+maplen = len(itemMap)
 def toRef(x):
 	a = []
 	for y in x:
 		if y in itemMap:
 			a.append(itemMap[y])
+		else:
+			idx = random.randint(0, maplen)
+			a.append(itemMap.iloc[idx])
+
+	if len(a) < 25:
+		for i in range(0, 25-len(a)):
+			idx = random.randint(0, maplen)
+			a.append(itemMap.iloc[idx])
 	return a
 		#else:
 		#	a.append(itemMap[y])
@@ -169,22 +196,48 @@ def toRef(x):
 
 intermediate_layer_model = Model(inputs=model.input,
                                  outputs=model.get_layer("final_dense").output)
-output_array = intermediate_layer_model.predict([items[:-1], actions[:-1]])
-
-#output_array = get_softmax_output([items[:-1], actions[:-1]])[0]
-#output_array = model.predict([items[:-1], actions[:-1]])
-outputs = list(map(lambda x: sorted(range(len(x)), key=lambda i: x[i], reverse=True)[:25], output_array))
-outputRef = list(map(toRef, outputs))
 #print(len(data), len(output_array), len(outputs))
 #print(outputs)
 fout = open("submit.csv", "w")
 fout.write("user_id,session_id,timestamp,step,item_recommendations\n")
-for i in range(1, len(data)):
-	row = data.iloc[i]
+
+titems = np.array(valid.item_idx.values.tolist())
+
+tactions = np.array(valid.action_list.values.tolist())
+
+stime = time.time()
+#total = int(len(titems) / 40)
+print("test total: ", total)
+#total = 175
+for start in range(0, total, 100):
+	#sys.stdout.write(f"{start} / {total}\t\t{time.time()-stime}\r")
+	end = start + 100
+	if end > total:
+		end = total
 	
-	fout.write(f"{row.user_id},{row.session_id},{row.timestamp},{row.step},")
-	for ref in outputRef[i-1]:
-		fout.write(f"{ref} ")
+	output_array = intermediate_layer_model.predict([titems[start:end], tactions[start:end]])
+
+	#output_array = get_softmax_output([items[:-1], actions[:-1]])[0]
+	#output_array = model.predict([items[:-1], actions[:-1]])
+	outputs = list(map(lambda x: sorted(range(len(x)), key=lambda i: x[i], reverse=True)[:25], output_array))
+	outputRef = list(map(toRef, outputs))
+
+	for i in range(start, end):
+		row = valid.iloc[i]
+		if row.reference == -1:
+			
+			#fout.write()
+			if i > 0:
+				ref_str = ""
+			
+				for ref in outputRef[i-start-1]:
+					ref_str = ref_str + str(ref) + " "
+				out_str = f"{row.user_id},{row.session_id},{row.timestamp},{row.step},"
+			
+				fout.write(out_str + ref_str)
+				fout.write('\n')
+
+	sys.stdout.write(f"{end} / {total}\t\t{time.time()-stime}\r")
 #print(zip(*heapq.nlargest(25, enumerate(output_array), key=operator.itemgetter(1)))[0])
 
 # outputs = list(map(lambda x: x.sort(reverse=True)[:25]))
